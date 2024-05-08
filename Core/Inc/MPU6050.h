7603b28805c90f018a9050ff5ec8f28b1ab7d419
seyitkadir <seyitkadir15@gmail.com>
2024-05-08 18:51:48 +0300

    MPU6050
    add mpu6050 library

---
+++ Core/Inc/MPU6050.h
@@ -0,0 +1,13 @@
+/*
+ * MPU6050.h
+ *
+ *  Created on: May 8, 2024
+ *      Author: seyit
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+
+
+#endif /* INC_MPU6050_H_ */
---
+++ Core/Src/MPU6050.c
@@ -0,0 +1,8 @@
+/*
+ * MPU6050.c
+ *
+ *  Created on: May 8, 2024
+ *      Author: seyit
+ */
+
+
